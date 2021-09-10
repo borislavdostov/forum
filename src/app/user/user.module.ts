8c6e23647b1be6126e271a3b64edb927e8f650a2
Borislav Dostov <bdostov@gmail.com>
2021-09-10 17:13:55 +0300

    User routing

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { UserService } from './user.service';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   providers: [
     UserService
