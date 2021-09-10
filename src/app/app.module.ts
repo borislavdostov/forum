ed737767756b7f7bdb762d40cbb7bc71575e63b8
Borislav Dostov <bdostov@gmail.com>
2021-09-10 16:17:18 +0300

    Bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './components/app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ThemeListComponent } from './components/theme-list/theme-list.component';
-import { AsideComponent } from './components/aside/aside.component';
-import { ThemeListItemComponent } from './components/theme-list-item/theme-list-item.component';
+import { AppComponent } from './app.component';
+import { ThemeListComponent } from './theme-list/theme-list.component';
+import { ThemeListItemComponent } from './theme-list-item/theme-list-item.component';
+import { HomeComponent } from './home/home.component';
+
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 import { UserService } from './services/user.service';
 import { storageServiceProvider } from './services/storage.service';
 import { ThemeService } from './services/theme.service';
 import { PostService } from './services/post.service';
-import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     ThemeListComponent,
-    AsideComponent,
     ThemeListItemComponent,
-    HomeComponent
+    HomeComponent,
+    PostListComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { IPost } from 'src/app/interfaces/post';
-import { PostService } from 'src/app/services/post.service';
+import { IPost } from '../interfaces';
+import { PostService } from '../services/post.service';
 
 @Component({
-  selector: 'app-aside',
-  templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.css']
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
-export class AsideComponent implements OnInit {
+export class PostListComponent implements OnInit {
 
   postList!: IPost[];
 
