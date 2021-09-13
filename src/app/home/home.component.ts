030d8404733a843bd5dc4444f95a3d6c2f727a83
Borislav Dostov <bdostov@gmail.com>
2021-09-13 10:53:35 +0300

    Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements DoCheck {
 
-  constructor() { }
+  isLogged = false;
 
-  ngOnInit(): void {
+  constructor(private userService: UserService) { }
+
+  ngDoCheck(): void {
+    this.isLogged = this.userService.isLogged;
   }
 
 }
