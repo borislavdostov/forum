f8908063bf7c000520ba9e47ebe6cc6261944cf3
Borislav Dostov <bdostov@gmail.com>
2021-09-15 16:19:38 +0300

    Bug fixes

--- src/app/shared/email-validator.directive.ts
+++ src/app/shared/email-validator.directive.ts
@@ -1,9 +1,10 @@
 import { Directive } from '@angular/core';
-import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 import { emailValidator } from './validators';
 
 @Directive({
-  selector: '[ngModel][appEmailValidator]'
+  selector: '[ngModel][appEmailValidator]',
+  providers: [{ provide: NG_VALIDATORS, useExisting: EmailValidatorDirective, multi: true }]
 })
 export class EmailValidatorDirective implements Validator {
 
